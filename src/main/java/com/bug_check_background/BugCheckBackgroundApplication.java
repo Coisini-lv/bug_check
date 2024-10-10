8a8fa25907e0e00ba2ea5d705a7491a6a4347ab7
lvshuaihang <lvshuaihang@hmntech.com>
2024-10-10 19:30:47 +0800

    初始化bugCheck工程

---
+++ src/main/java/com/bug_check_background/BugCheckBackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.bug_check_background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class BugCheckBackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugCheckBackgroundApplication.class, args);
+    }
+
+}
